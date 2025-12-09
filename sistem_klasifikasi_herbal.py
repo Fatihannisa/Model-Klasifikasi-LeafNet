eeed783736bbe4f4e87883e8c572308827e003a3
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2025-12-09 23:30:43 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -209,7 +209,7 @@ components.html(f"""
              filter: drop-shadow(0px 0px 4px rgba(0,0,0,0.35));">
     </div>
     <hr>
-""", height=80)
+""", height=100)
 
 
 # -------------------------------
