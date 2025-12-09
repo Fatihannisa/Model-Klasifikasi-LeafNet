a81d44e35e9d89008b77035bb6d4c094a434fece
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2025-12-09 21:17:32 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -231,7 +231,7 @@ if st.session_state.page == "upload":
         ] 
         for col, path in zip(ex_cols, example_paths): 
             with col: 
-                st.image(path, width=120, use_container_width=False)
+                st.image(path, width=100)
 
 # =======================
 # === HALAMAN HASIL =====
