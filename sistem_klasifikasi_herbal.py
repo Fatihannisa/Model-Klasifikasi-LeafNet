40bb3adeea598d8f90b225e1f40fccd8c2a90165
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2025-12-09 13:38:43 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -277,11 +277,8 @@ elif st.session_state.page == "result":
     st.markdown(data["informasi"] if data else "Tidak ada informasi.", unsafe_allow_html=True)
 
     # === Tautan Artikel ===
-    st.markdown("<div class='section-title'>Link Artikel</div>", unsafe_allow_html=True)
-    st.markdown(
-        f"<a href='{data['tautan_artikel']}' target='_blank'>{data['tautan_artikel']}</a>"
-        if data else "Tidak ada link.",
-        unsafe_allow_html=True
+    st.markdown("<div class='section-title'>Link Artikel: <a href='{data['tautan_artikel']}' target='_blank'>{data['tautan_artikel']}</a>"
+        if data else "Tidak ada link.", </div>", unsafe_allow_html=True)
     )
 
     # === Tautan Jurnal Penelitian ===
